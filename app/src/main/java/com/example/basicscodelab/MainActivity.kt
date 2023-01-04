c3dce390f00d30b9bb6fd196767726dabf6dc9bb
NUREK03 <jakub.nurkiewicz.notatki@gmail.com>
2023-01-04 17:06:48 +0100

    end

--- app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -4,17 +4,22 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -51,6 +56,7 @@ private fun Greetings(
     //names: List<String> = listOf("World", "Compose")
     names: List<String> = List(1000) {"$it"} // lambda
 ){
+
     LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
         items(items = names){ name->
             Greeting(name = name)
@@ -58,29 +64,26 @@ private fun Greetings(
     }
 }
 
+
 @Composable
 private fun Greeting(name: String) {
     // Dzięki temu remember wartość jeśli już wcześniej istniała zostanie zapamiętana (podobnie jak w przypadku statica w c++)
     val expanded = remember {mutableStateOf(false)}
 
-    // Tworzenie animacji, Jeśli chcesz by po zjechaniu na dół i powrocie na góre stan został zachowany użyj rememberSaveable
-    val extraPadding by animateDpAsState(
-        if (expanded.value) 48.dp else 0.dp,
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
-        )
-    )
 
-    Surface(
-        color = MaterialTheme.colorScheme.primary,
+    Card(
+        colors = CardDefaults.cardColors(),
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ){
-        Row(modifier = Modifier.padding(24.dp)){
+        Row(
+            modifier = Modifier
+                .padding(24.dp)
+                .animateContentSize { initialValue, targetValue -> }
+        )
+        {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp)) // zabezpieczenie przed paddingiem mniejszym od 0 (crash aplikacji!)
             ){
                 Text(text = "Hello,")
                 Text(
@@ -89,12 +92,19 @@ private fun Greeting(name: String) {
                         fontWeight = FontWeight.ExtraBold // dopisanie do stylu
                     )
                 )
+                if(expanded.value){
+                    Text("Composeum ipsum")
+                }
             }
-            ElevatedButton(
+            IconButton(
                 // przyjmuje funkcję, a nie wartość!
-                onClick = { expanded.value = !expanded.value }
+                onClick = { expanded.value = !expanded.value },
+                modifier = Modifier.padding(4.dp)
             ) {
-                Text(if(expanded.value) "Show less" else "Show more")
+                Icon(
+                    if(expanded.value) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                    contentDescription = if(expanded.value) stringResource(R.string.show_less) else stringResource(R.string.show_more)
+                )
             }
         }
     }
